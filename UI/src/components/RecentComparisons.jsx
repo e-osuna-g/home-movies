f29e6e4558fb58269e56840b4da398b3ce2a0c7a
Ernesto Osuna <ernesto.osuna.garzon@gmail.com>
2026-03-07 20:10:25 -0600

    default view shown to let  know they can add comparisons

--- UI/src/components/RecentComparisons.jsx
+++ UI/src/components/RecentComparisons.jsx
@@ -2,6 +2,7 @@ import CircularProgress from "@mui/material/CircularProgress";
 import { useRecentComparisons } from "../Query/useRecentComparisons.js";
 import CompareItem from "./CompareItem.jsx";
 import Grid from "@mui/material/Grid";
+import Alert from "@mui/material/Alert";
 
 const ParentRecentItems = (theme) => ({
   [theme.breakpoints.down("md")]: {
@@ -10,6 +11,13 @@ const ParentRecentItems = (theme) => ({
 });
 export default function RecentComparisons() {
   const { isPending, data, isSuccess } = useRecentComparisons();
+  if (isSuccess && data.length == 0) {
+    return (
+      <Alert severity="info" sx={{ width: "100%" }}>
+        Create a comparison, it will be shown here.
+      </Alert>
+    );
+  }
   return (
     <Grid sx={ParentRecentItems} container spacing={10}>
       {isPending ? <CircularProgress /> : isSuccess
