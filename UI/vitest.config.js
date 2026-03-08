017cf64da17fe5641dec40df77125ee77bbb0431
Ernesto Osuna <ernesto.osuna.garzon@gmail.com>
2026-03-08 01:51:50 -0600

    test now should not break

--- UI/vitest.config.js
+++ UI/vitest.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from "vitest/config";
 import { playwright } from "@vitest/browser-playwright";
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
   test: {
     browser: {
       enabled: true,
       provider: playwright(),
-      headless: false,
+      headless: true,
       instances: [
         { browser: "chromium" },
       ],
