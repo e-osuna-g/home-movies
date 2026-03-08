0a1439547be1e575865a7a05b05a8bae7593a4f7
Ernesto Osuna <ernesto.osuna.garzon@gmail.com>
2026-03-07 20:09:39 -0600

    added SERVER_PORT

--- api/src/envVars.js
+++ api/src/envVars.js
@@ -1,6 +1,6 @@
 export const OMDB_API_KEY = process.env.OMDB_API;
 export const OMDB_URL = process.env.OMDB_URL;
-
+export const SERVER_PORT = process.env.SERVER_PORT;
 export const DB = {
   NAME: process.env.DB_NAME,
   USERNAME: process.env.DB_USERNAME,
--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,12 @@
+import { SERVER_PORT } from "./envVars.js";
 import { buildFastify } from "./fastifyBuilder.js";
 import { pino } from "./logger.js";
 const fastify = await buildFastify();
 
 try {
-  await fastify.listen({ port: 3000 });
-  pino.info("Listening on port 3000");
+  await fastify.listen({ port: SERVER_PORT });
+  pino.info("Listening on port:" + SERVER_PORT);
 } catch (err) {
-  pino.error(err, "Error while trying to bind to port 3000");
+  pino.error(err, "Error while trying to bind to port" + SERVER_PORT);
   process.exit(1);
 }
